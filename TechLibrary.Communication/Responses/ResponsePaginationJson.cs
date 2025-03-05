35b4a623071ac3c67e82035c201d4e5d505330f0
Otavio Rocha <111688702+OtavioRochaDeveloper@users.noreply.github.com>
2025-03-05 11:53:58 -0300

    Add files via upload

---
+++ TechLibrary.Communication/Responses/ResponsePaginationJson.cs
@@ -0,0 +1,8 @@
+﻿namespace TechLibrary.Communication.Responses
+{
+    class ResponsePaginationJson
+    {
+        public int PageNumber { get; set; }
+        public int TotalCount { get; set; }
+    }
+}
